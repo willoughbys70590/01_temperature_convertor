47b040b7ec931b81edb190f75e3fc62d89f3195e
willoughbys70590 <60673309+willoughbys70590@users.noreply.github.com>
2021-02-10 12:34:22 +1300

    Update 00_tkinter_template.py

--- 00_tkinter_template.py
+++ 00_tkinter_template.py
@@ -2,6 +2,14 @@ from tkinter import *
 import random
 
 
-class foo:
-    def __init__(self, parent):
+class Convertor:
+    def __init__(self,parent):
         print("hello world ")
+
+
+# main routine
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Temperture Convertor")
+    something = Convertor(root)
+    root.mainloop()
