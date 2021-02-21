34aaabfbd7ed50ec49da9db48bc75007cd1e9ad4
willoughbys70590 <60673309+willoughbys70590@users.noreply.github.com>
2021-02-22 09:51:10 +1300

    latest code

--- 02_Converter_GUI_v1.py
+++ 02_Converter_GUI_v1.py
@@ -49,20 +49,26 @@ class Converter:
 
         self.to_f_button = Button(self.conversion_buttons_frame,
                                   text="To Fahrenheit", font="Arial 10 bold",
-                                  bg="orchid1", padx=10, pady=10)
+                                  bg="Orchid1", padx=10, pady=10)
         self.to_f_button.grid(row=0, column=1)
 
         # Answer label (row 4)
         self.converted_label = Label(self.converter_frame, font="Arial 14 bold",
                                      fg="purple", bg=background_color,
                                      pady=10, text="Calculation History", width=15)
+        self.converted_label.grid(row=4)
+
+        # History / Help button frame (row 5)
+        self.hist_help_frame = Frame(self.converter_frame)
+        self.hist_help_frame.grid(row=5, pady=10)
+
+        self.calc_hist_button = Button(self.hist_help_frame, font="Arial 12 bold",
+                                       text="Calculation History", width=15)
         self.calc_hist_button.grid(row=0, column=0)
 
         self.help_button = Button(self.hist_help_frame, font="Arial 12 bold",
                                   text="Help", width=5)
         self.help_button.grid(row=0, column=1)
-
-        # History / Help button frame (row 5)
 
 
 # main routine
