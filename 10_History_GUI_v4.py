868da36fa01982f58a00648a04bb5cbda3519d33
willoughbys70590 <60673309+willoughbys70590@users.noreply.github.com>
2021-03-08 09:57:21 +1300

    update

--- 10_History_GUI_v4.py
+++ 10_History_GUI_v4.py
@@ -7,46 +7,45 @@ import random
 class Converter:
   def __init__(self):
 
-    # Formatting variables...
-    background_color = "light blue"
+        # Formatting variables...
+        background_color = "light blue"
 
-    # In actual program this is blank and is populated with user calculations
-    self.all_calc_list = ['0 degrees C is -17.8 degrees F', 
-                      '0 degrees C is 32 degrees F',
-                      '40 degrees C is 104 degrees F', 
-                      '40 degrees C is 4.4 degrees F',
-                      '12 degrees C is 53.6 degrees F', 
-                      '24 degrees C is 75.2 degrees F',
-                      '100 degrees C is 37.8 degrees F']
+        # In actual program this is blank and is populated with user calculations
+        self.all_calc_list = ['0 degrees C is -17.8 degrees F',
+                          '0 degrees C is 32 degrees F',
+                          '40 degrees C is 104 degrees F',
+                          '40 degrees C is 4.4 degrees F',
+                          '12 degrees C is 53.6 degrees F',
+                          '24 degrees C is 75.2 degrees F',
+                          '100 degrees C is 37.8 degrees F']
 
-    # Converter Main Screen GUI
-    self.converter_frame = Fram(width=300, height=300, bg=background_color,
-                                pady=10)
-    self.converter_frame.grid()
+        # Converter Main Screen GUI
+        self.converter_frame = Frame(width=300, height=300, bg=background_color,
+                                    pady=10)
+        self.converter_frame.grid()
 
-    # Tempertature Conversion Heading (row 0)
-    self.temp_converter_label = Label(self.converter_frame, text="Temperature Converter",
-                                      font=("Arial", "16", "bold"),
-                                      bg=background_color,
-                                      padx=10,
-                                      pady=10)
-    self.temp_converter_label.grid(row=0)
+        # Tempertature Conversion Heading (row 0)
+        self.temp_converter_label = Label(self.converter_frame, text="Temperature Converter",
+                                          font=("Arial", "16", "bold"),
+                                          bg=background_color,
+                                          padx=10,
+                                          pady=10)
+        self.temp_converter_label.grid(row=0)
 
-    # history Button (row 1)
-    self.history_button = Button(self.converter_frame,
-                                text="History",
-                                font=("Arial", "14"),
-                                padx=10,
-                                pady=10,
-                                command=self.history)
-    self.history_button.grid(row=1)
+        # history Button (row 1)
+        self.history_button = Button(self.converter_frame, text="History",
+                                    font=("Arial", "14"),
+                                    padx=10,
+                                    pady=10,
+                                    command=self.history)
+        self.history_button.grid(row=1)
 
   def history(self):
-    get_history = history(self)
+    get_history = History(self)
     get_history.history_text.configure(text="History text goes here")
 
 
-class history:
+class History:
   def __init__(self, partner):
 
     background = "#a9ef99"  # Pale green
@@ -91,7 +90,7 @@ class history:
     # History output goes here ... (row 2)
 
     # Export / Dissmiss Button Frame (row 3)
-    self.export_dismiss_frame = Frame(Self.history_frame)
+    self.export_dismiss_frame = Frame(self.history_frame)
     self.export_dismiss_frame.grid(row=3, pady=10)
 
     # Export Button
@@ -104,8 +103,8 @@ class history:
     self.dismiss_button = Button(self.export_dismiss_frame,
                                 text="Dismiss",
                                 font="Arial 12 bold",
-                                command=partial(self.close))
-    self.dismiss_button.grid(row0, column=1)
+                                command=partial(self.close_history, partner))
+    self.dismiss_button.grid(row=0, column=1)
 
   def close_history(self, partner):
     # Put history button back to normal...
